3d31270825523b5ce5de534e0db4faeaa890cf0a
mihir-tandon <mihirtandon25@gmail.com>
2023-07-08 11:20:26 +0530

    path fixed

--- pages/kpi.py
+++ pages/kpi.py
@@ -6,7 +6,7 @@ import plotly.express as px
 #loading the data
 @st.cache_data
 def load_data():
-    path='data\kc_house_data.csv'
+    path='data/kc_house_data.csv'
     df = pd.read_csv(path)
     return df
 
